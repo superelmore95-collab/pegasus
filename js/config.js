c9ee0f12d053b50b5631186a5ef4944af6882d00
superelmore95-collab <super.elmore95@gmail.com>
2025-08-27 03:33:27 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js
+window.PEGASUS_CONFIG = {
+  API_BASE: 'https://pegasus-backend.super-elmore95.workers.dev',
+  JWT_SECRET: 'pegasus-super-secret-key-change-in-production',
+  APP_VERSION: '1.0.0'
+};
